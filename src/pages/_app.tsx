3b7231f6cbc7974c87ccc910164a1054265359fe
Mariana Silva <mariana.silva@mobilwave.pt>
2023-11-15 05:18:34 +0000

    removendo comentario

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -42,7 +42,6 @@ export default function App({ Component, pageProps }: AppProps) {
                 <Head>
                     <title>Mamãe Radiante</title>
                     <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
-                    {/* <meta name="facebook-domain-verification" content="lugupjkdi297w32zr8z18fbydhzxl6" />                 */}
                     <meta name="facebook-domain-verification" content="r7v3nethi08xl2x6uqps82ilyj0bkz" />
                 </Head>
                 <LanguageContext.Provider value={{updateContext: updateSelectedLanguage, contextValue: defaultBrowserLanguage}}>
