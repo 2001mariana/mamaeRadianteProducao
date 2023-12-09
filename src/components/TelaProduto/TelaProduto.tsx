07a737d45c471bf8c0f9fc56242b3e5c32e7980c
Mariana Silva <mariana.silva@mobilwave.pt>
2023-12-09 22:49:17 +0000

    teste 2 correcao rota pagamento com parametro

--- src/components/TelaProduto/TelaProduto.tsx
+++ src/components/TelaProduto/TelaProduto.tsx
@@ -68,6 +68,11 @@ function TelaProduto({ebookAtual}: TelaProdutoProps) {
       const rotaCompleta = router.asPath;
       const uuidAtualNaRota = `/${ebookAtual.uuid}?`
       const apenasParametro = rotaCompleta.replace(uuidAtualNaRota, '?')
+
+      // console.log('rotaCompleta', rotaCompleta)
+      // console.log('uuidAtualNaRota', uuidAtualNaRota)
+      // console.log('apenasParametro', apenasParametro)
+
       setParametroFacebook(apenasParametro)
     }
 
@@ -90,6 +95,8 @@ function TelaProduto({ebookAtual}: TelaProdutoProps) {
   
   useEffect(() => {
     const urlComprarComParametro = `${ebookAtual.urlComprarProduto}${parametroFacebook}`
+    // console.log('urlComprarComParametro', urlComprarComParametro)
+
     setUrlComprarComParametro(urlComprarComParametro)
   },[parametroFacebook, urlComprarComParametro, ebookAtual.urlComprarProduto])
 
@@ -99,23 +106,23 @@ function TelaProduto({ebookAtual}: TelaProdutoProps) {
     <div className='ProdutoEspecifico' id={`headline--${ebookAtual.idStyledByProduct}`}>
 
     <Script
-                          id="fb-pixel"
-                          strategy="afterInteractive"
-                          dangerouslySetInnerHTML={{
-                          __html: `
-                          !function(f,b,e,v,n,t,s)
-                          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-                          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-                          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-                          n.queue=[];t=b.createElement(e);t.async=!0;
-                          t.src=v;s=b.getElementsByTagName(e)[0];
-                          s.parentNode.insertBefore(t,s)}(window, document,'script',
-                          'https://connect.facebook.net/en_US/fbevents.js');
-                          fbq('init', '802228641664490');
-                          fbq('track', 'PageView');                    
-                          `,
-                          }}
-                        />
+      id="fb-pixel"
+      strategy="afterInteractive"
+      dangerouslySetInnerHTML={{
+      __html: `
+        !function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+        n.queue=[];t=b.createElement(e);t.async=!0;
+        t.src=v;s=b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t,s)}(window, document,'script',
+        'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '802228641664490');
+        fbq('track', 'PageView');                    
+      `,
+      }}
+    />
 
       <ConfettiDisplay 
         title={ebookAtual.bonus.titleHeadlineParabens} 
