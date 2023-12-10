1249a05827a0383582860c842d89bcec115c9348
Mariana Silva <mariana.silva@mobilwave.pt>
2023-12-10 01:06:41 +0000

    removendo codigo comentado

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,29 +2,13 @@ import { controlBrowserLanguage } from '@/components/LanguageControl/LanguageCon
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
-// import { useRouter } from 'next/router';
 import { createContext, useEffect, useState } from 'react';
-// import * as fbq from '../lib/fpixel'
 import Script from 'next/script';
 
 export const LanguageContext = createContext<{updateContext: (formatedBrowserLanguage: "ES" | "ENG" | "PT") => void, contextValue: 'PT' | 'ES' | 'ENG' | ''}>({updateContext: () => {}, contextValue: ''});
 
 export default function App({ Component, pageProps }: AppProps) {
     const [defaultBrowserLanguage, setDefaultBrowserLanguage] = useState<'PT' | 'ES' | 'ENG' | ''>('')
-    // const router = useRouter()
-
-  // useEffect(() => {
-  //   fbq.pageview()
-
-  //   const handleRouteChange = () => {
-  //     fbq.pageview()
-  //   }
-
-  //   router.events.on('routeChangeComplete', handleRouteChange)
-  //   return () => {
-  //     router.events.off('routeChangeComplete', handleRouteChange)
-  //   }
-  // }, [router.events])
 
     useEffect(() => {
         const languageBrowser = window.navigator ? window.navigator.language : '';
@@ -47,24 +31,6 @@ export default function App({ Component, pageProps }: AppProps) {
                 <LanguageContext.Provider value={{updateContext: updateSelectedLanguage, contextValue: defaultBrowserLanguage}}>
                     <div className='App--content'>
                         <Component {...pageProps} />
-                        {/* <Script
-                          id="fb-pixel"
-                          strategy="afterInteractive"
-                          dangerouslySetInnerHTML={{
-                          __html: `
-                          !function(f,b,e,v,n,t,s)
-                          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-                          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-                          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-                          n.queue=[];t=b.createElement(e);t.async=!0;
-                          t.src=v;s=b.getElementsByTagName(e)[0];
-                          s.parentNode.insertBefore(t,s)}(window, document,'script',
-                          'https://connect.facebook.net/en_US/fbevents.js');
-                          fbq('init', '1277739633622360');
-                          fbq('track', 'PageView');                    
-                          `,
-                          }}
-                        /> */}
                         <Script src="https://www.googletagmanager.com/gtag/js?id=G-EVY1Q6GR61" />
                         <Script
                           id='google-analytics'
